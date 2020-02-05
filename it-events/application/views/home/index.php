aa9977c7c3f84ab34231427ef3a479422e3e56b1
ysupriatna <Yayat Supriatna>
2020-02-05 19:03:01 +0700

    add auth controller

--- it-events/application/views/home/index.php
+++ it-events/application/views/home/index.php
@@ -1,42 +1,12 @@
-<!-- Content Wrapper -->
-<div id="content-wrapper" class="d-flex flex-column">
+<!-- Begin Page Content -->
+<div class="container-fluid">
 
-    <!-- Main Content -->
-    <div id="content">
+    <!-- Page Heading -->
+    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
 
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-            <!-- Topbar Navbar -->
-            <ul class="navbar-nav ml-auto">
-
-
-
-            </ul>
-
-        </nav>
-        <!-- End of Topbar -->
-
-    </div>
-    <!-- End of Main Content -->
-
-    <!-- Footer -->
-    <footer class="sticky-footer bg-white">
-        <div class="container my-auto">
-            <div class="copyright text-center my-auto">
-                <span>Copyright &copy; Your Website 2019</span>
-            </div>
-        </div>
-    </footer>
-    <!-- End of Footer -->
 
 </div>
-<!-- End of Content Wrapper -->
+<!-- /.container-fluid -->
 
 </div>
-<!-- End of Page Wrapper -->
-
-<!-- Scroll to Top Button-->
-<a class="scroll-to-top rounded" href="#page-top">
-    <i class="fas fa-angle-up"></i>
-</a>
+<!-- End of Main Content -->
